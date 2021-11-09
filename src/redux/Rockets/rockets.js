713c8730a18339d49b6f0940f920d67cd95476a8
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-09 16:10:29 +0100

    Installed axios

--- src/redux/Rockets/rockets.js
+++ src/redux/Rockets/rockets.js
@@ -2,21 +2,6 @@ const ROCKET_SUCCESS = 'ROCKET_SUCCESS';
 
 const initialState = {
   rockets: [
-    {
-      title: 'Falcon 1',
-      description: 'Falcon 1 is a partially reusable expendable launch vehicle developed and manufactured by SpaceX.',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Falcon_1_Demo_Mission.jpg/220px-Falcon_1_Demo_Mission.jpg',
-    },
-    {
-      title: 'Falcon 2',
-      description: 'Falcon 1 is a partially reusable expendable launch vehicle developed and manufactured by SpaceX.',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Falcon_1_Demo_Mission.jpg/220px-Falcon_1_Demo_Mission.jpg',
-    },
-    {
-      title: 'Falcon 13',
-      description: 'Falcon 1 is a partially reusable expendable launch vehicle developed and manufactured by SpaceX.',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Falcon_1_Demo_Mission.jpg/220px-Falcon_1_Demo_Mission.jpg',
-    },
 
   ],
 };
