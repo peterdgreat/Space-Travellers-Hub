03a703ee6f5758cac71a65514f90bc028efb6595
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-10 16:40:09 +0100

    Navbar styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/NavBar.css';
 import { NavLink } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import planet from './planet.png';
@@ -7,23 +8,23 @@ const NavBar = () => (
   <nav className="navbar navbar-expand-lg  bg">
     <div className="container">
 
-      <NavLink className="navbar-brand" to="/">
-        <img src={planet} alt="Logo Img" width="54" height="54" className="d-inline-block align-text-top" />
-        Space Travelers Hub
+      <NavLink className="navbar-brand d-flex align-items-center " to="/">
+        <img src={planet} alt="Logo Img" width="54" height="54" />
+        <div className="ps-3">Space Travelers Hub </div>
       </NavLink>
 
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <GiHamburgerMenu />
       </button>
       <div className="collapse   navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav col-12">
-          <li className="nav-item bdr  d-flex align-items-center">
+        <ul className="navbar-nav col-12 d-flex-md justify-content-end align-items-center">
+          <li className="nav-item bdr  d-flex align-items-center px-3">
             <NavLink className="nav-Link pt-1" to="/">Rockets</NavLink>
           </li>
-          <li className="nav-item bdr d-flex align-items-center">
+          <li className="nav-item bdr d-flex align-items-center px-3 sp-b">
             <NavLink className="nav-Link categories pt-1" to="/missions">Missions</NavLink>
           </li>
-          <li className="nav-item bdr d-flex align-items-center">
+          <li className="nav-item bdr d-flex align-items-center px-3">
             <NavLink className="nav-Link categories pt-1" to="/profile">My Profile</NavLink>
           </li>
 
